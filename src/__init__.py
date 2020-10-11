d00f877c9fe953dc25ff8cd6c0fe05cabce23f0d
mink0212 <mink.lie0212@gmail.com>
2020-10-11 22:34:31 +0900

    azure monitor

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ The flask application package.
 
 from flask import Flask
 from os import environ
-from applicationinsights.requests import WSGIApplication
+#from applicationinsights.requests import WSGIApplication
 app = Flask(__name__, static_folder='html/static', template_folder='html/templates')
-app.wsgi_app = WSGIApplication(environ.get('APPINSIGHTS_INSTRUMENTATIONKEY'), app.wsgi_app)
+#app.wsgi_app = WSGIApplication(environ.get('APPINSIGHTS_INSTRUMENTATIONKEY'), app.wsgi_app)
 from src.pymod.endpoint.index import app
