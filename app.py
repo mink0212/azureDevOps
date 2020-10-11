1fd4da3a6fc7d26af852b8bb93a808773e4f893f
mink0212 <mink.lie0212@gmail.com>
2020-10-11 22:40:48 +0900

    add host

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 
-from flask import Flask, render_template, url_for
-
+from os import environ
 from src import app
 
 #app = Flask(__name__, static_folder='html/static', template_folder='html/templates')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
